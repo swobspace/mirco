9a1167f5f93d5310868d88ebb35a7925fc06e85b
Wolfgang Barth <wob@swobspace.net>
2021-05-16 13:29:54 +0200

    model Server ready

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,2 +1,13 @@
 class Server < ApplicationRecord
+  # -- associations
+  # -- configuration
+  # -- validations and callbacks
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :uid, uniqueness: { case_sensitive: false, allow_blank: true }
+
+  def to_s
+    "#{name}"
+  end
+
+
 end
--- spec/models/server_spec.rb
+++ spec/models/server_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Server, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+
+  it "should get plain factory working" do
+    f = FactoryBot.create(:server)
+    g = FactoryBot.create(:server)
+    expect(f).to be_valid
+    expect(g).to be_valid
+    expect(f).to validate_uniqueness_of(:name).case_insensitive
+    expect(f).to validate_uniqueness_of(:uid).case_insensitive
+  end
+
+  it "to_s returns value" do
+    f = FactoryBot.create(:server, name: "xyzmirth")
+    expect("#{f}").to match ("xyzmirth")
+  end
+
 end
