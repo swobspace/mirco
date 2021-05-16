6b3f3af2bcc2e4004c50910b055f5303db8073d0
Wolfgang Barth <wob@swobspace.net>
2021-05-16 12:02:00 +0200

    starting Mirco module

---
+++ config/initializers/01_mirco.rb
@@ -0,0 +1,105 @@
+module Mirco
+  CONFIGURATION_CONTROLLER = [
+    "servers"
+  ]
+  CONFIGFILE = File.join(Rails.root, 'config', 'mirco.yml')
+  if File.readable? CONFIGFILE
+    config = YAML.load_file(CONFIGFILE)
+  end
+  CONFIG = config || Hash.new
+
+  def self.devise_modules
+    if CONFIG['devise_modules'].present?
+      CONFIG['devise_modules']
+    else
+      [ :remote_user_authenticatable,
+        :database_authenticatable,
+        :registerable,
+        :recoverable,
+        :rememberable,
+        :trackable
+      ]
+    end
+  end
+
+  def self.ldap_options
+    if CONFIG['ldap_options'].present?
+      ldapopts = CONFIG['ldap_options']
+      if ldapopts.kind_of? Hash
+        ldapopts = [ldapopts]
+      end
+      ldapopts.each do |opts|
+        opts.symbolize_keys!
+        opts.each do |k,v|
+          opts[k] = opts[k].symbolize_keys if opts[k].kind_of? Hash
+        end
+      end
+    else
+      nil
+    end
+  end
+
+  def self.mail_from
+    if CONFIG['mail_from'].present?
+      CONFIG['mail_from']
+    else
+      'root'
+    end
+  end
+
+  def self.use_ssl
+    if CONFIG['use_ssl'].present?
+      CONFIG['use_ssl']
+    else
+      false
+    end
+  end
+
+  def self.remote_user
+    if CONFIG['remote_user'].present?
+      CONFIG['remote_user']
+    else
+      "REMOTE_USER"
+    end
+  end
+
+  def self.action_cable_allowed_request_origins
+    if CONFIG['action_cable_allowed_request_origins'].present?
+      Array(CONFIG['action_cable_allowed_request_origins'])
+    else
+      [ 'http://localhost', 'https://localhost' ]
+    end
+  end
+
+  def self.host
+    if CONFIG['host'].present?
+      CONFIG['host']
+    else
+      "localhost"
+    end
+  end
+
+  def self.script_name
+    if CONFIG['script_name'].present?
+      CONFIG['script_name']
+    else
+      "/"
+    end
+  end
+
+  def self.mail_to
+    if CONFIG['mail_to'].present?
+      Array(CONFIG['mail_to'])
+    else
+      []
+    end
+  end
+
+  ActionMailer::Base.default_url_options = {
+   host: self.host,
+   script_name: self.script_name
+  }
+  Rails.application.routes.default_url_options[:host] = self.host
+  Rails.application.routes.default_url_options[:script_name] = self.script_name
+
+end
---
+++ spec/models/mirco_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+  
+RSpec.describe Mirco, type: :model do
+  context" with empty Settings" do
+    before(:each) do
+      allow(Mirco::CONFIG).to receive(:[]).with('devise_modules').and_return(nil)
+      allow(Mirco::CONFIG).to receive(:[]).with('mail_from').and_return(nil)
+      allow(Mirco::CONFIG).to receive(:[]).with('use_ssl').and_return(nil)
+      allow(Mirco::CONFIG).to receive(:[]).with('host').and_return(nil)
+      allow(Mirco::CONFIG).to receive(:[]).with('script_name').and_return(nil)
+      allow(Mirco::CONFIG).to receive(:[]).with('mail_to').and_return(nil)
+
+      allow(Mirco::CONFIG).to receive(:[]).with('remote_user').and_return(nil)
+      allow(Mirco::CONFIG).to receive(:[]).with('action_cable_allowed_request_origins').and_return(nil)
+    end
+    it { expect(Mirco.devise_modules).to contain_exactly(
+                                           :remote_user_authenticatable,
+                                           :database_authenticatable,
+                                           :registerable,
+                                           :recoverable,
+                                           :rememberable,
+                                           :trackable
+                                         )}
+    it { expect(Mirco.host).to eq("localhost")}
+    it { expect(Mirco.script_name).to eq("/")}
+    it { expect(Mirco.mail_from).to eq('root') }
+    it { expect(Mirco.use_ssl).to be_falsey }
+    it { expect(Mirco.remote_user).to eq('REMOTE_USER') }
+    it { expect(Mirco.mail_to).to eq([]) }
+    it { expect(Mirco.action_cable_allowed_request_origins).to contain_exactly(
+         'http://localhost', 'https://localhost' ) }
+  end
+
+  describe "::ldap_options" do
+    context" with empty Settings" do
+      before(:each) do
+        allow(Mirco::CONFIG).to receive(:[]).with('ldap_options').and_return(nil)
+      end
+      it { expect(Mirco.ldap_options).to be_nil}
+    end
+
+    context" with existing Settings" do
+      let(:ldap_options) {{
+        "host"=>"192.0.2.71",
+        "port"=>3268,
+        "base"=>"dc=example,dc=com",
+        "auth"=>{
+           "method"=>:simple,
+           "username"=>"myldapuser",
+           "password"=>"myldappasswd"
+        }
+      }}
+      let(:ldap_options_ary) {[{
+        "host"=>"192.0.2.71",
+        "port"=>3268,
+        "base"=>"dc=example,dc=com",
+        "auth"=>{
+           "method"=>:simple,
+           "username"=>"myldapuser",
+           "password"=>"myldappasswd"
+        }
+      }]}
+      let(:ldap_options_sym) {[{
+        host: "192.0.2.71",
+        port: 3268,
+        base: "dc=example,dc=com",
+        auth: {
+           method: :simple,
+           username: "myldapuser",
+           password: "myldappasswd"
+        }
+      }]}
+      it "returns symbolized keys from Hash" do
+        allow(Mirco::CONFIG).to receive(:[]).with('ldap_options').
+          and_return(ldap_options)
+        expect(Mirco.ldap_options).to eq(ldap_options_sym)
+      end
+      it "returns symbolized keys from Array of Hashes" do
+        allow(Mirco::CONFIG).to receive(:[]).with('ldap_options').
+          and_return(ldap_options_ary)
+        expect(Mirco.ldap_options).to eq(ldap_options_sym)
+      end
+    end
+  end
+
+end
