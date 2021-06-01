906cc06fd5ae6f07e87ed7777da4f15c89304718
Wolfgang Barth <wob@swobspace.net>
2021-06-01 10:12:36 +0200

    minimal example for homepage

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders start page" do
+    render
+    expect(rendered).to match(/Home/)
+  end
 end
