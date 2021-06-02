e1e6e4f6aedd79f62043dafc2fbf998f0c8a173e
Wolfgang Barth <wob@swobspace.net>
2021-06-02 10:21:06 +0200

    add store accessors

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,6 +2,11 @@ class Server < ApplicationRecord
   # -- associations
   # -- configuration
   encrypts :api_password
+  store_accessor :properties, :server_uid
+  store_accessor :properties, :server_jvm
+  store_accessor :properties, :server_version
+  store_accessor :properties, :server_settings
+  store_accessor :properties, :system_info
 
   # -- validations and callbacks
   validates :name, presence: true, uniqueness: { case_sensitive: false }
