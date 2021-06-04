73c519365a0e7ea3e6e7a24041a7b142b9a39ded
Wolfgang Barth <wob@swobspace.net>
2021-06-04 11:33:27 +0200

    Timezone seems to work only from application.rb

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Mirco
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Berlin'
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,3 +1,2 @@
 Rails.application.config.i18n.available_locales = [:de, :en]
 Rails.application.config.i18n.default_locale = :de
-Rails.application.config.time_zone = 'Berlin'
