4adbb0c224bb8675a5add171c38e5dfa6ce112c2
Wolfgang Barth <wob@swobspace.net>
2021-06-06 10:49:29 +0200

    fix error case, use explicit render:

--- app/controllers/servers/channel_statistics_controller.rb
+++ app/controllers/servers/channel_statistics_controller.rb
@@ -5,14 +5,20 @@ class Servers::ChannelStatisticsController < ChannelStatisticsController
     result = Statistics::FetchAll.new(server: @server, create_channel: true).call
     unless result.success?
       @server.errors.add(:base, :invalid)
+      flash[:error] = "WARN:: fetch channel statistics failed, server: #{@server}"
+      Rails.logger.warn(flash[:error])
     end
-    respond_with(@server, action: :show)
+    respond_with(@server, location: location, render: 'servers/show')
   end
 
 private
 
   def set_server
     @server = Server.find(params[:server_id])
+  end
+
+  def location
+    server_path(@server)
   end
 end
 
