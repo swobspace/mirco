a966489ec539f92defa03fdcbd176ec5d0899896
Wolfgang Barth <wob@swobspace.net>
2021-06-06 12:23:49 +0200

    more error info

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -18,8 +18,12 @@ class ChannelsController < ApplicationController
     result = Channels::FetchAll.new(server: @server).call
     unless result.success?
       @server.errors.add(:base, :invalid)
+      flash[:error] = "WARN:: fetch channels failed, server: #{@server}"
+      flash[:error] += "<br/>"
+      flash[:error] += result.error_messages.join("<br/>")
+      Rails.logger.warn(flash[:error])
     end
-    respond_with(@server, action: :show)
+    respond_with(@server, render: 'servers/show')
   end
 
   # DELETE /channels/1
--- app/controllers/servers/channel_statistics_controller.rb
+++ app/controllers/servers/channel_statistics_controller.rb
@@ -6,6 +6,8 @@ class Servers::ChannelStatisticsController < ChannelStatisticsController
     unless result.success?
       @server.errors.add(:base, :invalid)
       flash[:error] = "WARN:: fetch channel statistics failed, server: #{@server}"
+      flash[:error] += "<br/>"
+      flash[:error] += result.error_messages.join("<br/>")
       Rails.logger.warn(flash[:error])
     end
     respond_with(@server, location: location, render: 'servers/show')
