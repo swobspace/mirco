994b65f00420e57a5ed648bb82d190cf65504e34
Wolfgang Barth <wob@swobspace.net>
2021-06-07 09:34:16 +0200

    set sidekiq back to application path in application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,10 @@ module Mirco
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.after_initialize do
+      Rails.application.reload_routes!
+      Sidekiq::Web.app_url = Rails.application.routes.url_helpers.root_path
+    end
   end
 end
