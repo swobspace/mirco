88864fa41a46607d00b4f53a538e178040082355
Wolfgang Barth <wob@swobspace.net>
2021-06-07 15:52:55 +0200

    fix spec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
+  before(:each) do
+    @channel_statistics = FactoryBot.create_list(:channel_statistic, 2)
+  end
+
   it "renders start page" do
     render
-    expect(rendered).to match(/Home/)
+    expect(rendered).to match(/Queued Messages/)
   end
 end
