19247b2032c02dc5cd4b3f18d60187d5a95f165f
Wolfgang Barth <wob@swobspace.net>
2021-06-07 16:34:23 +0200

    sidekiq configuration stuff

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   unless Rails.env == 'test'
     config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_name_prefix = "mirco_" + Rails.env.to_s
   end
 end
