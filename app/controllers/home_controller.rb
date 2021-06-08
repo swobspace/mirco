9508c98a312e2b19f833cd319da11e9170e8ce93
Wolfgang Barth <wob@swobspace.net>
2021-06-08 10:01:27 +0200

    preparing for updating home page via turbo streams

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @channel_statistics = ChannelStatistic.where('channel_statistics.queued > 0').order('queued desc')
+    @queued_messages = ChannelStatistic.where('channel_statistics.queued > 0').order('queued desc')
   end
 end
