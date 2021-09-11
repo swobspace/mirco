2e8978c19469c03c6a69ce01d68bb972abf4176c
Wolfgang Barth <wob@swobspace.net>
2021-09-11 12:24:22 +0200

    add cssclass to td_expiration_component

--- app/components/td_expiration_component.rb
+++ app/components/td_expiration_component.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class TdExpirationComponent < ViewComponent::Base
-  def initialize(tstamp:, expiration_time:)
+  def initialize(tstamp:, expiration_time:, cssclass: 'text-center')
     @tstamp = tstamp
     @expiration_time = expiration_time
     @background_color = background_color
+    @cssclass = cssclass
   end
 
   private
-  attr_reader :tstamp, :expiration_time
+  attr_reader :tstamp, :expiration_time, :cssclass
 
   def background_color
     if tstamp <= expiration_time.before(Time.now)
