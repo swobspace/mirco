516af18cbb65926601ec870c0d6d529909f12a47
Wolfgang Barth <wob@swobspace.net>
2021-10-02 16:27:35 +0200

    bugfix

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,26 +3,19 @@ export default class extends Controller {
   static targets = ["content"]
   static classes = ["toggle"]
 
-  connect () {
-    // add your code here, if applicable
-  }
-
   toggle() {
-    // this.contentTarget.classList.toggle(this.toggleClass)
     this.contentTargets.forEach((t) => t.classList.toggle(this.toggleClass))
   }
 
   toggleAll(event) {
     const tclass = this.toggleClass
-    const applied = event.target.classList.contains(tclass)
     
     this.element.querySelectorAll('[data-toggle-target="content"]').forEach(function(t) {
-      if (applied) {
+      if (t.classList.contains(tclass)) {
         t.classList.remove(tclass)
       } else {
         t.classList.add(tclass)
       }
     })
   }
-
 }
