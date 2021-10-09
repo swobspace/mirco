3c14b1efcb06429bb593d970b32581ccf51b4b0a
Wolfgang Barth <wob@swobspace.net>
2021-10-09 10:56:02 +0200

    add 30min increase deltas as decorator for channel

--- spec/decorators/channel_decorator_spec.rb
+++ spec/decorators/channel_decorator_spec.rb
@@ -1,22 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe ChannelDecorator do
-  let(:ch)    { channel.decorate }
-  let(:properties) {{
-    "sourceConnector": {
-      "transportName": "rheinschipper",
-    }
-  }}
+  let(:server)  { FactoryBot.create(:server) }
+  let(:channel) { FactoryBot.create(:channel, server_id: server.id) }
+  let(:ch)      { channel.decorate }
 
-  describe "with properties set" do
-    let(:channel) { FactoryBot.create(:channel, properties: properties) }
-    it { expect(ch.sc_transport_name).to eq("rheinschipper") }
+  describe "without counters" do
+    let(:ch)      { channel.decorate }
+    describe "#sent_last_30min" do
+        it "doesn't raise an error" do
+          expect {
+          ch.sent_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.sent_last_30min).to eq(0) }
+    end
+
+    describe "#received_last_30min" do
+      let(:channel) { FactoryBot.create(:channel)}
+        it "doesn't raise an error" do
+          expect {
+          ch.received_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.received_last_30min).to eq(0) }
+    end
+
+    describe "#error_last_30min" do
+      let(:channel) { FactoryBot.create(:channel)}
+        it "doesn't raise an error" do
+          expect {
+          ch.error_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.error_last_30min).to eq(0) }
+    end
+
+    describe "#filtered_last_30min" do
+      let(:channel) { FactoryBot.create(:channel)}
+        it "doesn't raise an error" do
+          expect {
+          ch.filtered_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.filtered_last_30min).to eq(0) }
+    end
   end
 
-  describe "with empty properties" do
-    let(:channel) { FactoryBot.create(:channel)}
-  end
+  describe "with counters present" do
+    let!(:cc1) { FactoryBot.create(:channel_counter,
+      channel_id: channel.id, server_id: channel.server.id,
+      sent: 100,
+      received: 101,
+      filtered: 102,
+      error: 103,
+      queued: 0,
+      created_at: 10.minutes.before(Time.now)
+    )}
+    let!(:cc2) { FactoryBot.create(:channel_counter,
+      channel_id: channel.id, server_id: channel.server.id,
+      sent: 200,
+      received: 202,
+      filtered: 204,
+      error: 206,
+      queued: 5,
+      created_at: 5.minutes.before(Time.now)
+    )}
+    let!(:cc3) { FactoryBot.create(:channel_counter,
+      channel_id: channel.id, server_id: channel.server.id,
+      sent: 300,
+      received: 303,
+      filtered: 306,
+      error: 309,
+      queued: 5,
+      created_at: Time.now
+    )}
+    describe "#sent_last_30min" do
+      let(:channel) { FactoryBot.create(:channel)}
+      it "doesn't raise an error" do
+        expect {
+        ch.sent_last_30min
+      }.not_to raise_error
+      end
+      it { expect(ch.sent_last_30min).to eq(200) }
+    end
 
-  
+    describe "#received_last_30min" do
+        it "doesn't raise an error" do
+          expect {
+          ch.received_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.received_last_30min).to eq(202) }
+    end
+
+    describe "#error_last_30min" do
+        it "doesn't raise an error" do
+          expect {
+          ch.error_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.error_last_30min).to eq(206) }
+    end
+
+    describe "#filtered_last_30min" do
+        it "doesn't raise an error" do
+          expect {
+          ch.filtered_last_30min
+        }.not_to raise_error
+      end
+      it { expect(ch.filtered_last_30min).to eq(204) }
+    end
+  end
 end
-
