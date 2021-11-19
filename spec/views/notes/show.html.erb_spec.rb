44514e99e8664c55b9b122a9bd724a084ecbd248
Wolfgang Barth <wob@swobspace.net>
2021-11-19 16:21:40 +0100

    fix note views and specs

--- spec/views/notes/show.html.erb_spec.rb
+++ spec/views/notes/show.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "notes/show", type: :view do
   let(:user) { FactoryBot.create(:user) }
+  let!(:time) { Time.now }
   before(:each) do
     @ability = Object.new
     @ability.extend(CanCan::Ability)
@@ -10,18 +11,18 @@ RSpec.describe "notes/show", type: :view do
     allow(controller).to receive(:action_name) { "show" }
     @notable = FactoryBot.create(:server, name: "xyzmirth")
 
-    @note = assign(:note, FactoryBot.create(:note, server_id: @notable.id))
-
     @note = assign(:note, FactoryBot.create(:note,
       server_id: @notable.id,
       channel_id: nil,
       user_id: user.id,
-      type: "note"
+      type: "note",
+      created_at: time,
     ))
   end
 
   it "renders attributes in <p>" do
     render
+    expect(rendered).to match(/#{time.to_formatted_s(:db)}/)
     expect(rendered).to match(/xyzmirth/)
     expect(rendered).to match(//)
     expect(rendered).to match(/#{user}/)
