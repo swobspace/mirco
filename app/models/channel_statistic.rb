bc041bd70c735d82ab3226a29752fa230e7470a3
Wolfgang Barth <wob@swobspace.net>
2021-11-21 09:37:11 +0100

    example for non belongs_to association

--- app/models/channel_statistic.rb
+++ app/models/channel_statistic.rb
@@ -2,6 +2,14 @@ class ChannelStatistic < ApplicationRecord
   # -- associations
   belongs_to :server
   belongs_to :channel
+  # has_many :channel_counters, ->(cs) {
+  #   unscope(:where).where(
+  #     "server_id = :server_id and channel_id = :channel_id and meta_data_id = :meta_id",
+  #      server_id: cs.server_id,
+  #      channel_id: cs.channel_id,
+  #      meta_id: cs.meta_data_id
+  #   )
+  # }
   # -- configuration
   # -- validations and callbacks
   validates :server_id, :server_uid, presence: true
