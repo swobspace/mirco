4fc0545583f7da56741949bdc8e9022dcada0b66
Wolfgang Barth <wob@swobspace.net>
2021-11-22 18:23:01 +0100

    cleanup webpacker

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'sass-rails', '>= 6'
-gem 'webpacker', '~> 5.0'
+# gem 'webpacker', '~> 5.0'
 # Use Redis adapter to run Action Cable in production
 gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,18 @@
-// Load all the controllers within this directory and all subdirectories. 
-// Controller files must be named *_controller.js.
-
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
+import { Application } from "@hotwired/stimulus"
 const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+
+// Configure Stimulus development experience
+application.warnings = true
+application.debug = false
+window.Stimulus = application
+
+export { application }
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import DatatablesController from "./datatables_controller"
+application.register("datatables", DatatablesController)
+
+import ToggleController from "./toggle_controller"
+application.register("toggle", ToggleController)
