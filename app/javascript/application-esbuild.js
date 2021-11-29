4e987589d4855827278c0456bb9b928faff7bbdd
Wolfgang Barth (wob42) <wob@swobspace.net>
2021-11-29 15:53:55 +0100

    cleanup a bit

--- app/javascript/application-esbuild.js
+++ app/javascript/application-esbuild.js
@@ -5,19 +5,7 @@ import * as ActiveStorage from "@rails/activestorage"
 // import "./channels"
 
 import * as bootstrap from "bootstrap"
-// import '@fortawesome/fontawesome-free/js/all'
-// import "@fortawesome/fontawesome-free/css/all.css"
-
 import "chartkick/chart.js"
-
-// import JSZip from 'jszip'
-// require('pdfmake')
-// require('datatables.net-bs5')
-// require('datatables.net-buttons')
-// require('datatables.net-buttons-bs5')
-// require('datatables.net-buttons/js/buttons.colVis.js')
-// require('datatables.net-buttons/js/buttons.html5.js')
-// require('datatables.net-buttons/js/buttons.print.js')
 
 require('select2/dist/js/select2')
 
