402ad188d651c8c4622b22c3c7d865521c8442d0
Wolfgang Barth <wob@swobspace.net>
2021-12-12 16:29:27 +0100

    remove migration

--- db/migrate/20211212131239_set_condition_to_channel_statistic.rb
+++
@@ -1,7 +0,0 @@
-class SetConditionToChannelStatistic < ActiveRecord::Migration[6.1]
-  def change
-    ChannelStatistic.where(condition: '').each do |cs|
-      cs.update(condition: 'ok')
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_131239) do
+ActiveRecord::Schema.define(version: 2021_12_12_122657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
