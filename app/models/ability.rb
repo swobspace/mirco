16287b302720f7b6613165728b294ed2536e1e7a
Wolfgang Barth <wob@swobspace.net>
2022-03-27 14:17:04 +0200

    add navigate ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,10 +16,11 @@ class Ability
       can :manage, :all
       cannot %i[update destroy], :roles, ro: true
     elsif @user.role?(:manager)
-      can :read, :all
+      can [:read, :navigate], :all
       can :manage, [Note], user_id: @user.id
     elsif @user.authorities.any? || @user.group_authorities.any?
       can :read, [Home, Alert, Note, Server, Channel, ChannelStatistic]
+      can :navigate, :all
     else
       can :read, Home
     end
