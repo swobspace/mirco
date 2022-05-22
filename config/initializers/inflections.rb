90a3e2bba5b088d078f9161809476e8b9c279998
Wolfgang Barth <wob@swobspace.net>
2022-05-22 11:48:47 +0200

    reorg inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,11 +11,12 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.uncountable %(software)
-end
-
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.uncountable %(software)
+end
+
