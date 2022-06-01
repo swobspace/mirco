2e66dd69e1fd15f2a121d7d2228934d3bb98c2c9
Wolfgang Barth <wob@swobspace.net>
2022-06-01 18:48:43 +0200

    set source and destination connector relations

--- app/models/software_connection.rb
+++ app/models/software_connection.rb
@@ -1,5 +1,7 @@
 class SoftwareConnection < ApplicationRecord
-  belongs_to :source_connector
-  belongs_to :destination_connector
+  belongs_to :source_connector, -> { where(type: 'TxConnector') },
+             class_name: "InterfaceConnector"
+  belongs_to :destination_connector, -> { where(type: 'RxConnector') },
+             class_name: "InterfaceConnector"
   has_rich_text :description
 end
