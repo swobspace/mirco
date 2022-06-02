8f8f44055284f696af0360491adccf8bf11c95c9
Wolfgang Barth <wob@swobspace.net>
2022-06-02 12:07:39 +0200

    styling views

--- spec/views/software_connections/edit.html.erb_spec.rb
+++ spec/views/software_connections/edit.html.erb_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "software_connections/edit", type: :view do
   before(:each) do
-    @software_connection = assign(:software_connection, SoftwareConnection.create!(
-      source_connector: nil,
-      source_url: "MyString",
-      destination_connector: nil,
-      destination_url: "MyString",
-      ignore: false,
-      ignore_reason: "MyString",
-      description: nil
-    ))
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { 'software_connection' }
+    allow(controller).to receive(:action_name) { 'edit' }
+
+    @software_connection = assign(:software_connection, FactoryBot.create(:software_connection))
   end
 
   it "renders the edit software_connection form" do
@@ -18,18 +16,13 @@ RSpec.describe "software_connections/edit", type: :view do
 
     assert_select "form[action=?][method=?]", software_connection_path(@software_connection), "post" do
 
-      assert_select "input[name=?]", "software_connection[source_connector_id]"
-
+      assert_select "select[name=?]", "software_connection[location_id]"
+      assert_select "select[name=?]", "software_connection[source_connector_id]"
       assert_select "input[name=?]", "software_connection[source_url]"
-
-      assert_select "input[name=?]", "software_connection[destination_connector_id]"
-
+      assert_select "select[name=?]", "software_connection[destination_connector_id]"
       assert_select "input[name=?]", "software_connection[destination_url]"
-
       assert_select "input[name=?]", "software_connection[ignore]"
-
       assert_select "input[name=?]", "software_connection[ignore_reason]"
-
       assert_select "input[name=?]", "software_connection[description]"
     end
   end
--- spec/views/software_connections/new.html.erb_spec.rb
+++ spec/views/software_connections/new.html.erb_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "software_connections/new", type: :view do
   before(:each) do
-    assign(:software_connection, SoftwareConnection.new(
-      source_connector: nil,
-      source_url: "MyString",
-      destination_connector: nil,
-      destination_url: "MyString",
-      ignore: false,
-      ignore_reason: "MyString",
-      description: nil
-    ))
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { 'software_connection' }
+    allow(controller).to receive(:action_name) { 'new' }
+
+    @software_connection = assign(:software_connection, FactoryBot.build(:software_connection))
   end
 
   it "renders new software_connection form" do
@@ -18,18 +16,13 @@ RSpec.describe "software_connections/new", type: :view do
 
     assert_select "form[action=?][method=?]", software_connections_path, "post" do
 
-      assert_select "input[name=?]", "software_connection[source_connector_id]"
-
+      assert_select "select[name=?]", "software_connection[location_id]"
+      assert_select "select[name=?]", "software_connection[source_connector_id]"
       assert_select "input[name=?]", "software_connection[source_url]"
-
-      assert_select "input[name=?]", "software_connection[destination_connector_id]"
-
+      assert_select "select[name=?]", "software_connection[destination_connector_id]"
       assert_select "input[name=?]", "software_connection[destination_url]"
-
       assert_select "input[name=?]", "software_connection[ignore]"
-
       assert_select "input[name=?]", "software_connection[ignore_reason]"
-
       assert_select "input[name=?]", "software_connection[description]"
     end
   end
