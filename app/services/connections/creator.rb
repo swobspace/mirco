ba93db7ec776a67316eadeece9a8154af7f06a2a
Wolfgang Barth <wob@swobspace.net>
2022-06-06 14:49:04 +0200

    show channels within mirth

--- app/services/connections/creator.rb
+++ app/services/connections/creator.rb
@@ -15,9 +15,11 @@ module Connections
     end
 
     def save
-      build_connection_attributes(source_channel)
+      build_connection_attributes(source_channel, [source_channel.id])
       connection_attributes.each do |attributes|
-        software_connection = SoftwareConnection.find_or_create_by(attributes)
+        software_connection = SoftwareConnection
+                              .create_with(attributes['channel_ids'])
+                              .find_or_create_by(attributes.except('channel_ids'))
         Mirco::ConnectionDiagram.new(software_connection).delete
       end
     end
@@ -33,17 +35,19 @@ module Connections
       end
     end
 
-    def build_connection_attributes(channel)
+    def build_connection_attributes(channel, channel_ids)
       return if channel.nil?
       channel.destination_connectors.each do |dst| 
         next if dst.enabled == 'false'
         if dst.url.present?
           @connection_attributes << { location_id: location.id,
                                       source_url: source_url,
+                                      channel_ids: channel_ids,
                                       destination_url: dst.url }
         elsif dst.destination_channel_id.present?
           ch = Channel.where(id: dst.destination_channel_id).first
-          build_connection_attributes(ch)
+          channel_ids << ch.id
+          build_connection_attributes(ch, channel_ids)
         end
       end
     end
