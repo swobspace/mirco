cb35f99a932799a57e11ca94cbfd632be9dc4d31
Wolfgang Barth <wob@swobspace.net>
2022-08-06 12:48:59 +0200

    create a null connector model for connections without assigned connector

---
+++ app/models/null_interface_connector.rb
@@ -0,0 +1,18 @@
+# used in puml graphs
+class NullInterfaceConnector
+  attr_reader :name, :type, :url
+
+  def initialize(options: {})
+    @options = options.symbolize_keys
+    @name = @options.fetch(:name) { "Unknown" }
+    @type = @options.fetch(:type)
+    @url  = @options.fetch(:url)
+  end
+
+  InterfaceConnector.attribute_names.each do |attr_name|
+    next if ["id", "name", "type", "url", "created_at", "updated_at"].include?(attr_name)
+    define_method attr_name.to_sym do
+      nil
+    end
+  end
+end
