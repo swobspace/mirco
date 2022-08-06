84e93da592df9194b61537b2a39ee7b23575a6be
Wolfgang Barth <wob@swobspace.net>
2022-08-06 12:49:43 +0200

    add srv_identifier and srv_text

--- app/components/puml/multiple_connections_server_component.rb
+++ app/components/puml/multiple_connections_server_component.rb
@@ -8,4 +8,13 @@ class Puml::MultipleConnectionsServerComponent < ViewComponent::Base
   private
   attr_reader :server
 
+  def srv_identifier
+    "server_#{server.id}"
+  end
+
+  def srv_text
+    raw("<size:16><b>#{server.name}</b></size>" + '\n' +
+        "<color:grey>#{server.hostname} (#{server.ipaddress})</color>")
+  end
+
 end
