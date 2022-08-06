e8e598fa8876f18974dca09ef249a27e656752b8
Wolfgang Barth <wob@swobspace.net>
2022-08-06 17:37:09 +0200

    rename!

--- app/components/puml/multiple_connections_connector_component.rb
+++ app/components/puml/multiple_connections_connector_component.rb
@@ -5,4 +5,35 @@ class Puml::MultipleConnectionsConnectorComponent < ViewComponent::Base
     @connector = connector
   end
 
+  private
+  attr_reader :connector
+
+  def conn_identifier
+    "connector_#{connector.id}"
+  end
+
+  def conn_text
+    if connector.kind_of?(InterfaceConnector)
+      raw("<size:16><b>#{connector.name}</b></size>" + '\n' +
+          "<color:black>#{connector.sw_name} / #{connector.if_name}</color>" + '\n' +
+          "<color:grey>#{connector.hostname} (#{connector.ipaddress})</color>")
+    else
+      "not configured"
+    end
+  end
+
+  def conn_url
+    if connector.kind_of?(InterfaceConnector)
+      interface_connector_path(connector)
+    else
+      "#"
+    end
+  end
+
+  def conn_color
+    unless connector.kind_of?(InterfaceConnector)
+      "#AAAAAA"
+    end
+  end
+
 end
