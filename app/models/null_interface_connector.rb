2c556f4fc6ef2b18b8fb79910bb28cf136278d69
Wolfgang Barth <wob@swobspace.net>
2022-08-07 11:50:26 +0200

    set host if applicable

--- app/models/null_interface_connector.rb
+++ app/models/null_interface_connector.rb
@@ -1,6 +1,6 @@
 # used in puml graphs
 class NullInterfaceConnector
-  attr_reader :name, :type, :url, :server_id
+  attr_reader :name, :type, :url, :server_id, :host
   alias_attribute :id, :object_id
 
   def initialize(options = {})
@@ -9,11 +9,32 @@ class NullInterfaceConnector
     @type = @options.fetch(:type)
     @url  = @options.fetch(:url)
     @server_id  = @options.fetch(:server_id)
+    @host = fetch_host(@url)
+  end
+
+  def hostname
+    host&.name
+  end
+
+  def ipaddress
+    host&.ipaddress.to_s
   end
 
   InterfaceConnector.attribute_names.each do |attr_name|
     next if ["id", "name", "type", "url", "server_id", "created_at", "updated_at"].include?(attr_name)
     define_method attr_name.to_sym do
+      nil
+    end
+  end
+
+  private
+
+  def fetch_host(url)
+    return nil if type == 'TxConnector'
+    host = Mirco::Uri.new(url).host
+    if !!(host =~ Regexp.union([Resolv::IPv4::Regex, Resolv::IPv6::Regex]))
+      Host.where(ipaddress: host).first
+    else
       nil
     end
   end
