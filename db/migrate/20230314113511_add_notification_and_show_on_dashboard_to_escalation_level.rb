7af29c281fedf6953e2d3e45ed6b78262a2982f2
Wolfgang Barth (wob42) <wob@swobspace.net>
2023-03-14 12:36:37 +0100

    add show_on_dashboard and belongs_to:escalation_level

---
+++ db/migrate/20230314113511_add_notification_and_show_on_dashboard_to_escalation_level.rb
@@ -0,0 +1,7 @@
+class AddNotificationAndShowOnDashboardToEscalationLevel < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :escalation_levels, :notification_group, null: false, foreign_key: true
+    add_column :escalation_levels, :show_on_dashboard, :boolean, default: false
+    add_index :escalation_levels, :show_on_dashboard
+  end
+end
