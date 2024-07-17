9405e9e2d309945fda9af9ed1818b16e41805409
Wolfgang Barth <wob@swobspace.net>
2024-07-17 09:58:32 +0200

    avoid deprecation warnings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,7 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f 
 RSpec.configure do |config|
   config.include Capybara::DSL
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
