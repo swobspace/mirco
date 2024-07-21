862c3929612292ac8e6ced26cde471f05dd627d6
Wolfgang Barth <wob@swobspace.net>
2024-07-21 16:58:56 +0200

    fix ActionMailer smtp_settings (finally)

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,10 +1,8 @@
 if Rails.env.production? 
-  Rails.application.configure do
-    if Mirco.smtp_settings.nil?
-      config.action_mailer.delivery_method = :file
-    else
-      config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = Mirco.smtp_settings
-    end
+  if Mirco.smtp_settings.nil?
+    ActionMailer::Base.delivery_method = :file
+  else
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = Mirco.smtp_settings
   end
 end
