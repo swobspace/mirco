d310dc43e03887379cd492a9f9dbf2ccee70f2ae
Wolfgang Barth <wob@swobspace.net>
2024-10-12 17:03:35 +0200

    add acknowledge buttons from cocard

---
+++ app/components/acknowledge_button_component.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class AcknowledgeButtonComponent < ViewComponent::Base
+  # include Rails.application.routes.url_helpers
+  def initialize(notable:, readonly: true, small: true)
+    @notable = notable
+    @readonly = readonly
+    @type = 'acknowledge'
+    @current = notable.current_acknowledge
+    @small = small
+  end
+
+  def btn_size
+    if small
+      "btn-sm"
+    else
+      "btn"
+    end
+  end
+
+  def button_css
+    if @current.nil?
+      "btn #{btn_size} btn-warning"
+    else
+      "btn #{btn_size} btn-outline-secondary"
+    end
+  end
+
+  def button_action
+    if @current.nil?
+      new_polymorphic_path([notable, :note], type: type)
+    else
+      polymorphic_path([notable, current])
+    end
+  end
+
+  def render?
+    (!!current || !readonly) && error_state
+  end
+
+  private
+  attr_reader :notable, :type, :readonly, :current, :small
+
+  def error_state
+    notable.condition >= EscalationLevel::WARNING
+  end
+
+end
