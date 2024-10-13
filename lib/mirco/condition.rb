b4be163e26f827c2825c983657cd8ef5c3397546
Wolfgang Barth <wob@swobspace.net>
2024-10-13 16:23:24 +0200

    remove state text

--- lib/mirco/condition.rb
+++ lib/mirco/condition.rb
@@ -4,7 +4,7 @@ module Mirco
     def set_condition(state, message)
       self[:condition] = state
       self[:condition_message] =  [ shortcut(state),
-                                    I18n.t(state, scope: 'mirco.condition'),
+                                    # I18n.t(state, scope: 'mirco.condition'),
                                     message
                                   ].compact.join(' ')
       if state == Mirco::States::OK
