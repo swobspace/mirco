81a0ea00f1fa72a66524844c14939fed2bde4f19
Wolfgang Barth <wob@swobspace.net>
2024-10-18 17:47:05 +0200

    update spec - new naming

--- spec/components/ping_component_spec.rb
+++ spec/components/ping_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe HostPingComponent, type: :component do
+RSpec.describe PingComponent, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
