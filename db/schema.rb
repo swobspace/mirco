9f4b7b71f9558058a245f8b9c080036658fda228
Wolfgang Barth <wob@swobspace.net>
2024-12-14 17:38:34 +0100

    update to rails 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_27_133800) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_133800) do
   create_schema "_timescaledb_cache"
   create_schema "_timescaledb_catalog"
   create_schema "_timescaledb_config"
